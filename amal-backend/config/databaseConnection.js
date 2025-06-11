3cab6e3d18125552a4124c7e5552a6eeede69610
mohammed arshid <mohammedarshid234@gmail.com>
2025-06-11 15:09:51 +0530

    atlas

--- amal-backend/config/databaseConnection.js
+++ amal-backend/config/databaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const databaseConnnection = () => {
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(process.env.DB_URI || 'mongodb://localhost:27017/ebs' )
 .then((data) => {
     console.log(`Database is connected with ${data.connection.host}`);
     
